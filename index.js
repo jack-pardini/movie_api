dd5f1be95a4e85a1c675d6ea89d6ce0c7a5289f6
jack-pardini <jack.pardini@gmail.com>
2024-06-05 13:04:58 -0700

    exercise 2.4 work

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
-const express = require('express'),
-  morgan = require('morgan');
+const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
-// 
+app.arguments(bodyParser.json());
 
+
+// Movies
 let topTenMovies = []
 
+// Express static function
+app.use(express.static('public'));
+
+// Middleware
+app.use(morgan('common'));
+
+// GET requests
 app.get('/movies', (req, res) => {
   res.json(topTenMovies);
 });
 
 app.get('/', (req, res) => {
-  res.send('Default text Response.');
+  res.send('Welcome to my App!');
 });
 
+app.get('/documentation'), (req, res) => {
+    res.sendFile('public/documentation.html')
+}
+
+// error handler
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+// listen for requests
 app.listen(8080, () => {
   console.log('Your app is listening on port 8080.');
 });
