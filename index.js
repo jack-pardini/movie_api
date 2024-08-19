f03b00a8db5bc3f7628d025b107788a84cc814f0
jack-pardini <jack.pardini@gmail.com>
2024-08-19 16:51:50 -0700

    Update index.js

--- index.js
+++ index.js
@@ -20,25 +20,25 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 const cors = require('cors');
-// app.use(cors());
+app.use(cors());
 
 // Replacement for app.use(cors()); when you only want to allow requests from specific origins - for testing
-let allowedOrigins = ['http://localhost8080', 'http://testsite.com', '*'];
+// let allowedOrigins = ['http://localhost8080', 'http://testsite.com', '*'];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1) { //If a specific origin isn't found on the list of allowed origins
-      let message = 'The CORS policy for this application does not allow access from origin ' + origin;
-      return callback (new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if(!origin) return callback(null, true);
+//     if(allowedOrigins.indexOf(origin) === -1) { //If a specific origin isn't found on the list of allowed origins
+//       let message = 'The CORS policy for this application does not allow access from origin ' + origin;
+//       return callback (new Error(message ), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
 
-  let auth = require('./auth')(app);
-  const passport = require('passport');
-  require('./passport');
+let auth = require('./auth')(app);
+const passport = require('passport');
+require('./passport');
 
 // Express static function
 app.use(express.static('public'));
